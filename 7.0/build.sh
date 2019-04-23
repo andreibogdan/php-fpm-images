ec4f095f5260bc57039c5aeff385200964e98a82
Andrei B <abogdan@optaros.com>
2019-04-23 17:55:26 +0300

    Add new fixes

--- 7.0/build.sh
+++ 7.0/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+set -x
 docker build --rm -t optaros/php-fpm-7.0:latest .
 docker push optaros/php-fpm-7.0:latest
--- 7.2/build.sh
+++ 7.2/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+set -x
 docker build --rm -t optaros/php-fpm-7.2:latest .
 docker push optaros/php-fpm-7.2:latest
