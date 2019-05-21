fd77798a689a7bfcd4b1e5758e55b0795684cb88
Andrei B <abogdan@optaros.com>
2019-05-21 14:34:38 +0300

    Add build with no cache

--- 7.1/build.sh
+++ 7.1/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -x
-docker build --rm -t optaros/php-fpm-7.1:latest .
+docker login
+docker build --no-cache --rm -t optaros/php-fpm-7.1:latest .
 docker push optaros/php-fpm-7.1:latest
